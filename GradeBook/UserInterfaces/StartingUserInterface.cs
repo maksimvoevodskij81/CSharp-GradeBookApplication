5c1a415f74528c1335d353f7d3a0c5f320b30307
Maksym Voievodskyi <maksimvoevodskij81@gmail.com>
2023-01-24 15:43:30 +0100

    fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,12 +41,14 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             BaseGradeBook gradeBook = null;
-            if (parts[3] == "standard")
-                 gradeBook = new StandardGradeBook(name);
-            else if (parts[3] == "ranked")
-                gradeBook = new RankedGradeBook(name);
-            else
+            if (parts[3] != "standard" || parts[3] != "ranked")
                 Console.WriteLine(" is not a supported type of gradebook, please try again");
+            if (parts[3] == "ranked")
+                gradeBook = new RankedGradeBook(name);
+            if (parts[3] == "standard")
+                gradeBook = new StandardGradeBook(name);
+
+
 
                 Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
